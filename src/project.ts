84788c7cb118357a00dcf674b02742699dae0e77
Alexander Tsepkov <atsepkov@gmail.com>
2025-10-25 13:42:17 -0400

    Add application scene displaying application checklist

--- src/project.ts
+++ src/project.ts
@@ -2,8 +2,9 @@ import {makeProject} from '@motion-canvas/core';
 
 import tag from './scenes/tag?scene';
 import task from './scenes/task?scene';
+import application from './scenes/application?scene';
 import externalApi from './scenes/external_api?scene';
 
 export default makeProject({
-  scenes: [tag, task, externalApi],
+  scenes: [tag, task, application, externalApi],
 });
